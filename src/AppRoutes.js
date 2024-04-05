5b6075555f35e967a2c7c0a6156c5189a3f18c89
Khanyisile Ngoma <ngomakhanyi88@gmail.com>
2024-04-05 21:55:10 +0200

    Updated react project

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/Home/HomePage';
 import FoodPage from './pages/Food/FoodPage';
-import CartPage from './pages/Cart/CartPage';
 
 export default function AppRoutes() {
   return (
@@ -11,7 +10,6 @@ export default function AppRoutes() {
     <Route path="/search/:searchTerm" element= {<HomePage />} />
     <Route path="/tag/:tag" element= {<HomePage />} />
     <Route path="/food/:id" element= {<FoodPage />} />
-    <Route path="/cart" element= {<CartPage />} />
 
   </Routes>
   );
--- src/pages/Cart/CartPage.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import classes from './cartPage.module.css';
-
-export default function CartPage() {
-  return <div>CartPage</div>;
-}
